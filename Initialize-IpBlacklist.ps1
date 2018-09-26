13e7bc4b6a12ed060b15e0dba599a574d5d1c49e
Cory Murphy <cmurphy2266@gmail.com>
2018-09-26 07:56:29 -0400

    Update Initialize-IpBlacklist.ps1

--- Initialize-IpBlacklist.ps1
+++ Initialize-IpBlacklist.ps1
@@ -1,12 +1,50 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 
-$installPath = New-Item -Path ($env:ProgramFiles) -Name 'Powershell.IpBlacklist' -ItemType Directory -Force -ErrorAction SilentlyContinue;
+$type = @"
+
+using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace PowershellIpBlackList
+{
+    public static class Utils
+    {
+        const int MAX_PATH = 255;
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetShortPathName(
+            [MarshalAs(UnmanagedType.LPTStr)]
+            string path,
+            [MarshalAs(UnmanagedType.LPTStr)]
+            StringBuilder shortPath,
+            int shortPathLength
+            );
+
+        public static string GetShortPath(string path) {
+            var shortPath = new StringBuilder(MAX_PATH);
+            GetShortPathName(path, shortPath, MAX_PATH);
+            return shortPath.ToString();
+        }
+    }
+}
+"@
+
+Add-Type -TypeDefinition $type
+
+$name = 'Powershell.IpBlacklist';
+
+$programFilesShortName = [PowershellIpBlackList.Utils]::GetShortPath($env:ProgramFiles);
+
+$installPath = New-Item -Path ($programFilesShortName) -Name $name -ItemType Directory -Force -ErrorAction SilentlyContinue;
 
 $scriptPath = [System.IO.Path]::Combine($installPath.FullName, 'Invoke-IpBlacklistJobCached.ps1');
 
 Invoke-WebRequest -Uri:'https://raw.githubusercontent.com/corymurphy/Powershell.IpBlacklist/master/Invoke-IpBlacklistJobCached.ps1' -OutFile:$scriptPath
 
-$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument $scriptPath -WorkingDirectory 'C:\Program Files\Powershell.IpBlacklist';
+$working = [System.IO.Path]::Combine($programFilesShortName, $name));
+
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument $scriptPath -WorkingDirectory $working;
 
 $triggerDefault = New-ScheduledTaskTrigger -AtStartup ;
 
